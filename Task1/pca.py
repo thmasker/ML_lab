4e6a5124acd5d2671d824d434a362dd533393d3b
Alberto Velasco Mata <Alberto.Velasco1@alu.uclm.es>
2019-10-31 07:09:14 +0100

    [Task1] PCA cleanup & interpretation

--- Task1/pca.py
+++ Task1/pca.py
@@ -1,35 +1,49 @@
 import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import os
 
 
-df = pd.read_csv("T2_features.csv")
+FEATURES_FILE = os.path.join('.', 'T2_features.csv')
+
+
+print(f"Loading extracted features from {FEATURES_FILE}")
+df = pd.read_csv(FEATURES_FILE)
 df = df.dropna()
 
 
-from sklearn import preprocessing 
-
-
+##  Normalization
 scaler = preprocessing.StandardScaler()
 df_scaled = scaler.fit_transform(df)
 
-from sklearn.decomposition import PCA
 
+##  PCA
 estimator = PCA(n_components = 2)
 X_pca = estimator.fit_transform(df_scaled)
+print("[ Explained variance ratio ]")
 print(estimator.explained_variance_ratio_)
 print(sum(estimator.explained_variance_ratio_))
+# Interpretation:
+#   We got a total explained variance of around 78-79%, which is higher
+#   than 75%, so these two components are representative
 
 
-import matplotlib.pyplot as plt
-import numpy
-numbers = numpy.arange(len(X_pca))
-fig, ax = plt.subplots()
-for i in range(len(X_pca)):
-    plt.plot(X_pca[i][0], X_pca[i][1]) 
-# for i in range(len(X_pca)):
-#    plt.text(X_pca[i][0], X_pca[i][1], numbers[i]) 
-# # be careful with xlim and ylim
-# plt.xlim(-1, 4)
-# plt.ylim(-0.2, 1)
-ax.grid(True)
-fig.tight_layout()
+print("[ Relation between PCA components and features ]")
+print(pd.DataFrame(np.matrix.transpose(estimator.components_), columns=['PC-1', 'PC-2'], index=df.columns))
+# Interpretation:
+#   - Component1 includes mainly contributions from X and Y axes
+#   - Component2 includes mainly contributions from Z axis
+
+
+##  Plot
+plt.scatter(X_pca[:,0], X_pca[:,1])
+plt.xlabel("PC-1")
+plt.ylabel("PC-2")
+plt.grid(True)
 plt.show()
+plt.tight_layout()
+# Interpretation:
+#   - Since Component2 represents mostly Z axes and there's an outlier,
+#       the phone might have fallen
