bba9ab69fb965c0c9ccb684ed81247b483dfb2fe
thmasker <diego.p.hidalgo@gmail.com>
2019-12-09 17:35:21 +0100

    [src] Training and testing division. 80% --> training; 20% --> testing

---
+++ Task3/src/train_test_division.py
@@ -0,0 +1,51 @@
+import os
+import pandas as pd
+import random
+
+'''
+Training data --> 80 % attacks + 80 % not attacks
+Test data --> 20 % attacks + 20 % not attacks
+'''
+THRESHOLD = 0.8
+
+df = pd.read_csv(os.path.join('..', 'data', 'task3_dataset.csv'))
+pd.set_option('display.width', None)
+pd.set_option('display.max_columns', None)
+
+FEATURES = [
+	'UserID',
+	'UUID',
+	'Version',
+	'TimeStemp',
+	'GyroscopeStat_x_MEAN',
+	'GyroscopeStat_z_MEAN',
+	'GyroscopeStat_COV_z_x',
+	'GyroscopeStat_COV_z_y',
+	'MagneticField_x_MEAN',
+	'MagneticField_z_MEAN',
+	'MagneticField_COV_z_x',
+	'MagneticField_COV_z_y',
+	'Pressure_MEAN',
+	'LinearAcceleration_COV_z_x',
+	'LinearAcceleration_COV_z_y',
+	'LinearAcceleration_x_MEAN',
+	'LinearAcceleration_z_MEAN',
+	'attack']
+
+ATTACK = FEATURES.index('attack')
+
+train = []
+test = []
+
+for i, row in df.iterrows():
+	if random.random() < THRESHOLD:
+		train.append(row)
+	else:
+		test.append(row)
+
+
+df_train = pd.DataFrame(data=train, columns=FEATURES)
+df_test = pd.DataFrame(data=test, columns=FEATURES)
+
+df_train[FEATURES].to_csv(os.path.join('..', 'data', 'task3_train.csv'), index=False)
+df_test[FEATURES].to_csv(os.path.join('..', 'data', 'task3_test.csv'), index=False)
