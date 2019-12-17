b6fb4d1f738a09d30bfecfebc68941407c3afd13
Alberto Velasco Mata <Alberto.Velasco1@alu.uclm.es>
2019-12-17 12:08:59 +0100

    [data] Fix train/test split and organize dirs

--- Task3/src/train_test_division.py
+++ Task3/src/train_test_division.py
@@ -1,53 +1,29 @@
 import os
 import pandas as pd
+pd.set_option('display.width', None)
+pd.set_option('display.max_columns', None)
 import random
+from sklearn.model_selection import train_test_split
 
 '''
 Training data --> 80 % attacks + 80 % not attacks
 Test data --> 20 % attacks + 20 % not attacks
 '''
-THRESHOLD = 0.8
-
-df = pd.read_csv(os.path.join('.', 'data', 'task3_dataset.csv'))
-pd.set_option('display.width', None)
-pd.set_option('display.max_columns', None)
-
-FEATURES = [
-	'UserID',
-	'UUID',
-	'Version',
-	'TimeStemp',
-	'GyroscopeStat_x_MEAN',
-	'GyroscopeStat_z_MEAN',
-	'GyroscopeStat_COV_z_x',
-	'GyroscopeStat_COV_z_y',
-	'MagneticField_x_MEAN',
-	'MagneticField_z_MEAN',
-	'MagneticField_COV_z_x',
-	'MagneticField_COV_z_y',
-	'Pressure_MEAN',
-	'LinearAcceleration_COV_z_x',
-	'LinearAcceleration_COV_z_y',
-	'LinearAcceleration_x_MEAN',
-	'LinearAcceleration_z_MEAN',
-	'attack']
-
-attack_df = df[df['attack'] == 1]
-noattack_df = df[df['attack'] == 0]
+TRAIN = 0.8
+RAW_DATA = os.path.join('.', 'data', 'raw', 'dataset.csv')
 
 
-train = []
-test = []
+df = pd.read_csv(RAW_DATA)
 
-for i, row in df.iterrows():
-	if random.random() < THRESHOLD:
-		train.append(row)
-	else:
-		test.append(row)
+print("> Attack")
+attack_train, attack_test = train_test_split(df[df['attack'] == 1], train_size=TRAIN, shuffle=True)
+print("\tTrain:", len(attack_train))
+print("\tTest: ", len(attack_test))
 
+print("> No-Attack")
+noattack_train, noattack_test = train_test_split(df[df['attack'] == 0], train_size=TRAIN, shuffle=True)
+print("\tTrain:", len(noattack_train))
+print("\tTest: ", len(noattack_test))
 
-df_train = pd.DataFrame(data=train, columns=FEATURES)
-df_test = pd.DataFrame(data=test, columns=FEATURES)
-
-df_train[FEATURES].to_csv(os.path.join('.', 'data', 'task3_train.csv'), index=False)
-df_test[FEATURES].to_csv(os.path.join('.', 'data', 'task3_test.csv'), index=False)
+pd.concat([attack_train, noattack_train], ignore_index=True).to_csv(os.path.join('.', 'data', 'processed', 'train.csv'), index=False)
+pd.concat([attack_test, noattack_test], ignore_index=True).to_csv(os.path.join('.', 'data', 'processed', 'test.csv'), index=False)
